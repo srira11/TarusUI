ea9e546a33fd2c129383c09edc6936c0f8f9cab3
srira11 <sriram.v@rently.com>
2024-03-20 18:31:26 +0530

    updated hosted url

--- tarus.js
+++ tarus.js
@@ -1,3 +1,4 @@
+const HOSTED_URL = "https://main--zingy-ganache-c529c9.netlify.app";
 document.body.innerHTML += `<div id="tarus"></div>`;
 
 function tarusCSS() {
@@ -11,10 +12,10 @@ function tarusCSS() {
 
         #tarus-frame{
             border: none;
-            height: 650px;
-            width: 350px;
-            box-shadow: 0 0 40px -5px;
-            border-radius: 10px;
+            height: 632px;
+            width: 357px;
+            box-shadow: 0 10px 15px -3px rgba(16, 24, 40, 0.1), 0 4px 6px -4px rgba(16, 24, 40, 0.1);
+            border-radius: 8px;
             position: absolute;
             bottom: 0;
             right: 0;
@@ -35,6 +36,7 @@ function tarusCSS() {
             position: relative;
             z-index: 1001;
             color: black;
+            box-shadow: 0 10px 15px -3px rgba(16, 24, 40, 0.1), 0 4px 6px -4px rgba(16, 24, 40, 0.1);
         }
     </style>`;
 }
@@ -44,7 +46,7 @@ function chatButton(){
 }
 
 function tarusFrame() {
-    return `<iframe id="tarus-frame" src="https://srira11.github.io/"></iframe>`;
+    return `<iframe id="tarus-frame" src="${HOSTED_URL}"></iframe>`;
 }
 
 const tarus = document.querySelector('#tarus');
@@ -55,16 +57,19 @@ tarus.innerHTML += `<div id="tarus-chat-wrapper">
 </div>`;
 
 window.addEventListener('message', (event) => {
-    if(event.data === 'minimize')
+    if(event.data === 'minimize' && event.origin === HOSTED_URL) {
         document.querySelector('#tarus-frame').style.display = 'none';
+        document.querySelector('#tarus-chat-button').style.display = 'block';
+    }
 });
 
 let firstClick = true;
 document.querySelector('#tarus-chat-button').addEventListener('click', () => {
     const iframe = document.querySelector('#tarus-frame');
     if(firstClick === true) {
-        iframe.contentWindow.postMessage('initConnection', 'https://srira11.github.io/');
+        iframe.contentWindow.postMessage('initConnection', HOSTED_URL);
         firstClick = false;
     }
     iframe.style.display = 'block';
+    document.querySelector('#tarus-chat-button').style.display = 'none';
 });
