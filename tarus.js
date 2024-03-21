75afe8ade87463f78d763f0149e61578c2b3408c
srira11 <sriram.v@rently.com>
2024-03-21 09:45:34 +0530

    Removed console log

--- tarus.js
+++ tarus.js
@@ -57,7 +57,6 @@ tarus.innerHTML += `<div id="tarus-chat-wrapper">
 </div>`;
 
 window.addEventListener('message', (event) => {
-    console.log(event.origin);
     if(event.data === 'minimize' && event.origin === HOSTED_URL) {
         document.querySelector('#tarus-frame').style.display = 'none';
         document.querySelector('#tarus-chat-button').style.display = 'block';
